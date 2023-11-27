2d846074596f062589fa8b5521adef384865b20d
Albie-GH <134883999+Albie-GH@users.noreply.github.com>
2023-11-27 23:37:37 +1300

    Adding admin's user management options

---
+++ 106_A2_M1/ViewModel/AdminManagingUserVM.cs
@@ -0,0 +1,30 @@
+﻿using _106_A2_M1.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _106_A2_M1.ViewModel
+{
+    public class AdminManagingUserVM : AdminDashboardViewModel
+    {
+        
+        private Admin _admin;
+        private User _activeUser;
+        public User ActiveUser
+        {
+            get => _activeUser;
+            set
+            {
+                if(_activeUser != value)
+                {
+                    _activeUser = value;
+                    OnPropertyChanged(nameof(ActiveUser));
+                }
+            }
+        }
+
+
+    }
+}
